3f944f7fc7af2a48ef0bd6e501b3fb41a28f3cc9
PedroVMB <pedromb.dev@gmail.com>
2023-09-12 16:59:34 -0300

    feat: initial commit, upload infra project

---
+++ src/main/java/api/video_plataform/VideoPlataformApplication.java
@@ -0,0 +1,13 @@
+package api.video_plataform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoPlataformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoPlataformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/video_plataform/VideoPlataformApplicationTests.java
@@ -0,0 +1,13 @@
+package api.video_plataform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoPlataformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
